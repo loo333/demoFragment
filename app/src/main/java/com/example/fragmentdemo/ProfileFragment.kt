e6e95c504166d2d3505c719b010a43ce66536061
Loo Ming Yea <loomy-wp18@student.tarc.edu.my>
2021-02-16 21:56:43 +0800

    init

---
+++ app/src/main/java/com/example/fragmentdemo/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.example.fragmentdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class ProfileFragment :  Fragment()  {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.activity_profile_fragment, container, false)
+
+        val btnBack = v.findViewById<Button>(R.id.buttonBack)
+
+        btnBack.setOnClickListener {
+            activity!!.supportFragmentManager.popBackStack()
+        }
+
+        return v
+    }
+}
